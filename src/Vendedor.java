894c0efe06eebd827bd791ead50e3b8b5208e27c
ErickNeves07 <barbosa.erick.neves@gmail.com>
2025-06-06 08:57:04 -0300

    Criando classe vendedor com atributos e métodos de comportamento

---
+++ src/Vendedor.java
@@ -0,0 +1,45 @@
+public class Vendedor {
+    private long cpf;
+    private String nome;
+    private double valorVendas;
+    private double salarioBase;
+
+    public Vendedor(long cpf, String nome, double valorVendas, double salarioBase ){
+        this.cpf = cpf;
+        this.nome = nome;
+        this.valorVendas = valorVendas;
+        this.salarioBase = salarioBase;
+    }
+
+    public long getCpf() {
+        return cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public double getValorVendas() {
+        return valorVendas;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setSalarioBase(double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+
+    public void setValorVendas(double valorVendas) {
+        this.valorVendas = valorVendas;
+    }
+
+    public String toString() {
+        return "CPF ➜ "+this.cpf+"\nNome ➜ "+this.nome+"\nValor de vendas ➜ "+this.valorVendas+"\nSalário base ➜"+this.salarioBase;
+    }
+}
